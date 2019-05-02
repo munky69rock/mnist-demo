a06becae21cd9f20af51bab8751c5e62c8ce8b8f
Masayuki Uehara <munky69rock@gmail.com>
2019-05-02 14:33:18 +0900

    Formatted

--- index.py
+++ index.py
@@ -2,13 +2,16 @@ from flask import Flask, request, render_template, redirect, jsonify
 from PIL import Image
 import numpy as np
 from mnist import Classifier
+import better_exceptions
 
 app = Flask(__name__)
 classifier = Classifier()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -18,7 +21,7 @@ def predict():
     #image.save("num.png")
     image = image.convert('L')
     image = 1.0 - np.asarray(image, dtype="float32") / 255
-    image = image.reshape((1,784))
+    image = image.reshape((1, 784))
 
     prediction = classifier.predict(image)
-    return jsonify({ str(k): float(v * 100) for k,v in prediction.items() })
+    return jsonify({str(k): float(v * 100) for k, v in prediction.items()})
--- mnist.py
+++ mnist.py
@@ -1,19 +1,19 @@
 import tensorflow as tf
 
-class Classifier:
 
+class Classifier:
     def __init__(self):
         x = tf.placeholder(tf.float32, shape=[None, 784])
         self.x = x
         y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
-        W = tf.Variable(tf.zeros([784,10]))
+        W = tf.Variable(tf.zeros([784, 10]))
         b = tf.Variable(tf.zeros([10]))
 
         W_conv1 = self.weight_variable([5, 5, 1, 32])
         b_conv1 = self.bias_variable([32])
 
-        x_image = tf.reshape(x, [-1,28,28,1])
+        x_image = tf.reshape(x, [-1, 28, 28, 1])
 
         h_conv1 = tf.nn.relu(self.conv2d(x_image, W_conv1) + b_conv1)
         h_pool1 = self.max_pool_2x2(h_conv1)
@@ -27,7 +27,7 @@ class Classifier:
         W_fc1 = self.weight_variable([7 * 7 * 64, 1024])
         b_fc1 = self.bias_variable([1024])
 
-        h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
         keep_prob = tf.placeholder(tf.float32)
@@ -48,9 +48,9 @@ class Classifier:
         #prediction = tf.argmax(self.y_conv, 1)
         #return prediction.eval(feed_dict={self.x: image, self.keep_prob: 1.0}, session=self.sess)
 
-        feed_dict = { self.x: image, self.keep_prob: 1.0 }
+        feed_dict = {self.x: image, self.keep_prob: 1.0}
         classification = self.sess.run(self.y_conv, feed_dict)
-        return { i: v for i, v in enumerate(classification[0])}
+        return {i: v for i, v in enumerate(classification[0])}
 
     def weight_variable(self, shape):
         initial = tf.truncated_normal(shape, stddev=0.1)
@@ -64,8 +64,9 @@ class Classifier:
         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
     def max_pool_2x2(self, x):
-        return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                strides=[1, 2, 2, 1], padding='SAME')
+        return tf.nn.max_pool(
+            x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 
 if __name__ == '__main__':
     from PIL import Image
@@ -74,6 +75,6 @@ if __name__ == '__main__':
     classifier = Classifier()
     image = Image.open('./sample_2.png').convert('L')
     image = 1.0 - np.asarray(image, dtype="float32") / 255
-    image = image.reshape((1,784))
+    image = image.reshape((1, 784))
     prediction = classifier.predict(image)
     print(prediction)
